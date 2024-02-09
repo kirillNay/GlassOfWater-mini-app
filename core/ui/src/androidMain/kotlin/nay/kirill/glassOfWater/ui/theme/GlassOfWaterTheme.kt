3e3c1ce8ed38b78bd3c83619c2eeb59893335ac0
k.naydyuk <k.naydyuk@okko.tv>
2024-02-09 20:16:22 +0300

    Get rid of unused imports

--- core/ui/src/androidMain/kotlin/nay/kirill/glassOfWater/ui/theme/GlassOfWaterTheme.kt
+++ core/ui/src/androidMain/kotlin/nay/kirill/glassOfWater/ui/theme/GlassOfWaterTheme.kt
@@ -2,12 +2,8 @@ package nay.kirill.glassOfWater.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 
 @Composable
 actual fun GlassOfWaterTheme(
